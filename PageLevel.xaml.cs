c14475cc7a2241a2aa180613306f8c73d6e68dd2
wheezchiz <102015448+miguelcarl@users.noreply.github.com>
2023-09-15 11:39:53 +0800

    Add files via upload

---
+++ PageLevel.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace pdc60ResourceDictionary
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageLevel : ContentPage
+    {
+        public PageLevel()
+        {
+            InitializeComponent();
+        }
+    }
+}
